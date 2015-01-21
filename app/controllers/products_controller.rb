234dfcc4b37c0cebb3e6b1ee7208e34eb6d55cd8
R Hing <rhjunkmail@gmail.com>
2015-01-21 16:17:07 +0000

    define routes,create templates, define actions

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,35 @@
+class ProductsController < ApplicationController
+	
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+private
+
+	def product_params
+		# Whitelisted form data
+	end
+
+	def find_product
+		# Query to read a record from the db
+	end
+
+
+end
